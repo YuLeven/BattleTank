d631b54521dbfc47c6bcb0a95871143028180aae
Yuri Levenhagen <yurileven@gmail.com>
2017-09-12 21:06:35 -0300

    Moving on with the implementation

--- BattleTank/Source/BattleTank/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/TankBarrel.cpp
@@ -2,6 +2,14 @@
 
 #include "TankBarrel.h"
 
+UTankBarrel::UTankBarrel()
+{
+	//Turret's default settings
+	MaxDegreesPerSecond = 20.f;
+	MinEvelationDegrees = 0.f;
+	MaxElevationDegrees = 40.f;
+}
+
 void UTankBarrel::Elevate(float DegreesPerSecond)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Elevating..."))
--- BattleTank/Source/BattleTank/TankBarrel.h
+++ BattleTank/Source/BattleTank/TankBarrel.h
@@ -9,13 +9,31 @@
 /**
  * 
  */
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
 public:
 
+	//Class constructor. OCD tells me green stuff has to be here too.
+	UTankBarrel();
+
+	//Moves the barrel horizontally
 	void Elevate(float DegreesPerSecond);
+
+private:
+
+	//Sets up the maximum allowed speed of elevation for the barrel
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond;
+
+	//Determinates the maximum allowed elevation of aiming
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxElevationDegrees;
+
+	//Determinates the minimum allowed elevation of aiming
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MinEvelationDegrees;
 	
 };
