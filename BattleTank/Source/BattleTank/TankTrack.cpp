15dfaaad20d20222fdf586db0b8297ec92f40a3b
Yuri Levenhagen <yurileven@gmail.com>
2017-09-23 17:11:29 -0300

    Added early method to handle throttling

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -2,6 +2,19 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	TrackMaxDrivingForce = 650000.f;
+}
 
+void UTankTrack::SetThrottle(float Throttle)
+{ 
+	UE_LOG(LogTemp, Warning, TEXT("Applied throttle of %f to %s"), Throttle, *GetName())
+
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector TrackLocation = GetComponentLocation();
+	UPrimitiveComponent* RootTank = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	RootTank->AddForceAtLocation(ForceApplied, TrackLocation);
+}
 
 
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -14,7 +14,16 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+
+	UTankTrack();
+
+	// Adds throttle value from -1 to 1 to the track
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Determines how much force (in Newtons) can be applied when fully throttled
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce;
 	
 };
